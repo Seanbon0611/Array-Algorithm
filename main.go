9b38a84fd7c6352047fc197acf22d03c531fc181
Sean Dever <seand0611@gmail.com>
2020-11-29 01:17:46 +0000

    removed math package

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 /*
 Write a function that accepts two arrays
@@ -36,14 +33,14 @@ func same(arr1 []float64, arr2 []float64) bool {
   //iterate through first map
 	for key, _ := range firstArrayMap {
     //Checking to see if second map contains a value thats squared the first map's key
-		_, ok := secondArrayMap[math.Pow(key, 2)]
+		_, ok := secondArrayMap[key*key]
     //if does not contain the correlating squared element return false
 		if !ok {
 			fmt.Println("false, arrays do not contain correlating squared elements")
 			return false
 		}
     //Check to see if both maps are of the same frequency if not return false
-		if secondArrayMap[math.Pow(key, 2)] != firstArrayMap[key] {
+		if secondArrayMap[key*key] != firstArrayMap[key] {
       fmt.Println("false, arrays do not have the same frequency")
 			return false
 		}
